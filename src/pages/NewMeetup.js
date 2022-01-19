ac2724ad77046ab0f527f846d61fe5b3ede62e48
hassanedelbi <hassan.edelbi@gmail.com>
2022-01-19 16:16:49 +0200

    added nvigate(v6)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,28 @@
-import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 const NewMeetupPage = () => {
-	return <div>New Meetup Page</div>;
+	const navigate = useNavigate();
+	const addMeetupHandler = (meetupData) => {
+		fetch(
+			'https://react-meetup-manager-default-rtdb.firebaseio.com/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then(() => {
+			navigate('/');
+		});
+	};
+
+	return (
+		<section>
+			<h1>New Meetup Page</h1>
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
+		</section>
+	);
 };
 
 export default NewMeetupPage;
