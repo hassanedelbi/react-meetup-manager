03b133c0d607a149de5e3a3fa52cb20a92d9010f
hassanedelbi <hassan.edelbi@gmail.com>
2022-01-20 13:12:59 +0200

    worked on favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,23 @@
-import React from 'react';
-
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
 const FavoritesPage = () => {
-	return <div>Favorite Page</div>;
+	const favoritesCtx = useContext(FavoritesContext);
+
+	let content;
+
+	if (favoritesCtx.totalFavorites === 0) {
+		content = <p> No Favorites Yet</p>;
+	} else {
+		content = <MeetupList meetups={favoritesCtx.favorites} />;
+	}
+
+	return (
+		<section>
+			<h1>My Favorites</h1>
+			{content}
+		</section>
+	);
 };
 
 export default FavoritesPage;
