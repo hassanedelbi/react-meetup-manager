f58988f736306d2fdcac4461fbfc281816faad80
hassanedelbi <hassan.edelbi@gmail.com>
2022-01-20 13:13:23 +0200

    added favorite context provider to main app index js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Layout from './components/layout/Layout';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
+import { FavoritesContextProvider } from './store/favorites-context';
 function App() {
 	return (
-		<Layout>
-			<Routes>
-				<Route path='/' element={<AllMeetupsPage />} />
-				<Route path='/new-meetup' element={<NewMeetupPage />} />
-				<Route path='/favorites' element={<FavoritesPage />} />
-				Working
-			</Routes>
-		</Layout>
+		<FavoritesContextProvider>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<AllMeetupsPage />} />
+					<Route path='/new-meetup' element={<NewMeetupPage />} />
+					<Route path='/favorites' element={<FavoritesPage />} />
+					Working
+				</Routes>
+			</Layout>
+		</FavoritesContextProvider>
 	);
 }
 
